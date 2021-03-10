b7184660d752b4d53403042cf76ea15c1cfaf463
DESKTOP-1S608KI\WINDOWS <mittapabt2536@gmail.com>
2021-03-10 16:45:16 +0700

    update

--- app.js
+++ app.js
@@ -1,24 +1,40 @@
+// const express = require('express');
+// const app = express();
+
+
+// app.get('/' , (req ,res) => {
+//  res.status(200).json({
+//      status: 200 , 
+//      data: 'i am Index'
+//  })
+// });
+
+// app.get('/data' , (req ,res ) => {
+//    res.status(200).json({
+//        status: 200 ,
+//        data: 'I am data page'
+//    })
+
+// })
+
+
+// app.listen(8080 , 'localhost' , () => {
+  
+//     console.log('start running is port 8080');
+// });
+
+
+
 const express = require('express');
 const app = express();
 
-
-app.get('/' , (req ,res) => {
- res.status(200).json({
-     status: 200 , 
-     data: 'i am Index'
- })
-});
-
-app.get('/data' , (req ,res ) => {
+app.get('/' , (req , res) => {
    res.status(200).json({
-       status: 200 ,
-       data: 'I am data page'
+       status: '200' ,
+       data: 'I am homepage'
    })
-
-})
-
-
-app.listen(8080 , 'localhost' , () => {
-  
-    console.log('start running is port 8080');
 });
+
+app.listen(3000 , 'localhost' , () => {
+    console.log('start port running 3000');
+})
